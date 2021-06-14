c4944c8a664b4113f5881f6445d3e94cdab19c59
keyfpenc11 <cstylerb@gmail.com>
2021-06-14 08:47:40 +0300

    added some stuff in main script and created changelog file

--- simplefetch.sh
+++ simplefetch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo Simplefetch beta  v0.1
+echo Simplefetch beta  v0.2
 echo
 echo User: $USER
 echo Hostname: $HOSTNAME
 echo Uptime: "$(uptime -p)"
+echo 'Date (Local):' "$(date)"
+echo 'Date (UTF):' "$(date -u)"
+
 
