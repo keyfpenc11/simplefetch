be3a5339f8a99f6188b76864cf96fdd897d479b7
keyfpenc11 <cstylerb@gmail.com>
2021-06-16 05:43:04 +0000

    added kernel version

--- simplefetch.sh
+++ simplefetch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo Simplefetch beta  v0.2
+echo Simplefetch beta  v0.3
+echo
 echo
 echo User: $USER
 echo Hostname: $HOSTNAME
+echo
+echo Kernel version: "$(uname -r)"
+echo
 echo Uptime: "$(uptime -p)"
-echo 'Time (Local):' "$(date)"
-echo 'Time (UTF):' "$(date -u)"
-
+echo 'Date (Local):' "$(date)"
+echo 'Date (UTF):' "$(date -u)"
+echo
 
